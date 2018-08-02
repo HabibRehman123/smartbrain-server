9d855d80a54b0e4ddf0c876b7621500dca90ea82
Habib Rehman <HabibRehman@Habibs-MacBook-Pro.local>
2018-08-02 12:57:38 -0400

    adding the server again

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.post('/imageURL', (req, res) => { register.handleAPIcall(req, res)})
 
 
 
-app.listen(3000, ()=>{
-console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=>{
+console.log(`app is running on port ${process.env.PORT}`);
 })
