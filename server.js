3706840e2ac1686be2b6f952078eb87f580ee63c
Habib Rehman <HabibRehman@Habibs-MacBook-Pro.local>
2018-08-02 14:35:02 -0400

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const profile = require('./Components/profile');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'HabibRehman',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
