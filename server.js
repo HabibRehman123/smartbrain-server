dad10558ca47f107351c10bafc780ed36b37e472
Habib Rehman <40871363+HabibRehman123@users.noreply.github.com>
2018-09-23 21:34:35 -0400

    First commit

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ const profile = require('./Components/profile');
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl: true
+  	host:'127.0.0.1',
+  	user:'HabibRehman',
+  	password:'',
+  	database:'smart'
   }
 });
 
@@ -27,10 +29,8 @@ app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) =>{profile.handleProfile(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageURL', (req, res) => { register.handleAPIcall(req, res)})
+app.post('/imageURL', (req, res) => { image.handleAPIcall(req, res)})
 
-
-
-app.listen(process.env.PORT || 3000, ()=>{
-console.log(`app is running on port ${process.env.PORT}`);
+app.listen(3000, ()=>{
+console.log(`app is running on port 3000`);
 })
